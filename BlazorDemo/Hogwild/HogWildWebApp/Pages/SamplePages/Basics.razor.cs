003bc836c6d434e923d60a848b676197d7c0c4a0
James Thompson <jthompson@nait.ca>
2023-10-25 13:12:44 -0600

    Blazor Oct 25

--- BlazorDemo/Hogwild/HogWildWebApp/Pages/SamplePages/Basics.razor.cs
+++ BlazorDemo/Hogwild/HogWildWebApp/Pages/SamplePages/Basics.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using HogWildWebApp.Areas.ViewModels;
+using Microsoft.AspNetCore.Components;
 
 namespace HogWildWebApp.Pages.SamplePages
 {
@@ -11,6 +12,7 @@ namespace HogWildWebApp.Pages.SamplePages
         //  the odd or even value
         private int oddEvenValue;
         #endregion
+
         #region Text Boxes
         //  email address
         private string emailText;
@@ -22,12 +24,44 @@ namespace HogWildWebApp.Pages.SamplePages
 
         #region Radio Buttons, Check boxes & Text Area
         //  selected loopMeal
-        private string loopMeal = "breakfast";
+        private string meal = "breakfast";
         private string[] meals { get; set; } = new string[] {"breakfast", "lunch", "dinner", "snack"};
         //  used to hold the value of the acceptance
         private bool acceptanceBox;
         //  used to hold the balue for the message body
         private string messageBody;
+        #endregion
+
+        #region List and Sliders
+
+        //  pretend tat the following collection is data from a database
+        //  The collection is based on a 2 property class called SelectionList
+        //  The data for the list will be created in a separate method.
+        /// <summary>
+        /// Gets or sets a list of SelectionView objects representing various rides.
+        /// </summary>
+        private List<SelectionView> rides;
+
+        /// <summary>
+        /// Gets or sets the ID of the selected ride from the rides list.
+        /// </summary>
+        private int myRide;
+
+        /// <summary>
+        /// Gets or sets the user's chosen vacation spot as a string.
+        /// </summary>
+        private string vacationSpot;
+
+        /// <summary>
+        /// Gets or sets a list of strings representing various vacation spots.
+        /// </summary>
+        private List<string> vacationSpots;
+
+        /// <summary>
+        /// Gets or sets the review rating.
+        /// </summary>
+        /// <value>The review rating.</value>
+        private int reviewRating = 5;
         #endregion
         //  used to display any feedback to the end user.
         private string feedback;
@@ -41,6 +75,9 @@ namespace HogWildWebApp.Pages.SamplePages
 
             //  call the RandonValue method to perform custom initialization logic.
             RandomValue();
+
+            // Call the 'PopulatedList' method to populate predefined data for the list.
+            PopulatedList();
         }
 
         //  Generates a random number betwen 0 and 25 using the Random class
@@ -79,10 +116,65 @@ namespace HogWildWebApp.Pages.SamplePages
             InvokeAsync(StateHasChanged);
         }
 
-        //  Handle the selection of the loop loopMeal
-        private void HandleMealSelection(ChangeEventArgs e)
+        //  This method is called when a user submits radio, check box and area text.
+        private void RadioCheckAreaSubmit()
         {
-            loopMeal = e.Value.ToString();
+            // Combine various values and store them in the 'feedback' variable as a formatted string.
+            feedback = $"Meal {meal}; Acceptance {acceptanceBox}; Message {messageBody}";
+
+            // Trigger a UI update to reflect the changes made to the 'feedback' variable.
+            InvokeAsync(StateHasChanged);
+        }
+
+        /// <summary>
+        /// This method is called when the user submits the list and slider form.
+        /// It gathers user selections for 'myRide,' 'vacationSpot,' and 'reviewRating,'
+        /// and generates feedback based on these selections.
+        /// </summary>
+        private void ListSliderSubmit()
+        {
+            // Generate feedback string incorporating the selected values.
+            feedback = $"Ride {myRide}; Vacation {vacationSpot}; Review Rating {reviewRating}";
+
+            // Invoke asynchronous method 'StateHasChanged' to trigger a re-render of the component.
+            InvokeAsync(StateHasChanged);
+        }
+
+        /// <summary>
+        /// Populates the 'rides' list and 'vacationSpots' list with predefined data.
+        /// </summary>
+        private void PopulatedList()
+        {
+            int i = 1;
+
+            // Create a pretend collection from the database representing different types
+            // of transportation (rides).
+            rides = new List<SelectionView>();
+            rides.Add(new SelectionView() { ValueID = i++, DisplayText = "Car" });
+            rides.Add(new SelectionView() { ValueID = i++, DisplayText = "Bus" });
+            rides.Add(new SelectionView() { ValueID = i++, DisplayText = "Bike" });
+            rides.Add(new SelectionView() { ValueID = i++, DisplayText = "Motorcycle" });
+            rides.Add(new SelectionView() { ValueID = i++, DisplayText = "Boat" });
+            rides.Add(new SelectionView() { ValueID = i++, DisplayText = "Plane" });
+
+            // Sort the 'rides' list alphabetically based on the 'DisplayText' property.
+            rides.Sort((x, y) => x.DisplayText.CompareTo(y.DisplayText));
+
+
+            // Initialize and populate the 'vacationSpots' list with predefined vacation destinations.
+            vacationSpots = new List<string>();
+            vacationSpots.Add("California");
+            vacationSpots.Add("Caribbean");
+            vacationSpots.Add("Cruising");
+            vacationSpots.Add("Europe");
+            vacationSpots.Add("Florida");
+            vacationSpots.Add("Mexico");
+        }
+
+        //  Handle the selection of the loop loopMeal
+            private void HandleMealSelection(ChangeEventArgs e)
+        {
+            meal = e.Value.ToString();
         }
     }
 }
